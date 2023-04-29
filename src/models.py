0b8f86f94238ae0ff6539c3b323fd030639c3190
NVR-2023 <123973723+NVR-2023@users.noreply.github.com>
2023-04-29 12:42:48 +0000

    Tables and Diigram generated - 29/04/(2023

--- src/models.py
+++ src/models.py
@@ -1,32 +1,43 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+
+class NewUser(Base):
+    __tablename__ = 'new_user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250), nullable=False)
+    favourites = relationship("favourite")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False, unique=True)
+    alive = Column(Boolean, nullable=False)
+    favourites = relationship("favourite")
 
-    def to_dict(self):
-        return {}
 
-## Draw from SQLAlchemy base
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+    population = Column(Integer)
+    favourites = relationship("favourite")
+
+
+class Favourite(Base):
+    __tablename__ = 'favourite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("new_user.id"))
+    character_id = Column(Integer, ForeignKey("character.id"))
+    planet_id = Column(Integer, ForeignKey("planet.id"))
+
+
 render_er(Base, 'diagram.png')
